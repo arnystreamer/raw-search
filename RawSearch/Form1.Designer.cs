c36823667abc7fbbcc4eeab6a1c6b91e0ec93b23
Artur Usmanov <arnystreamer@gmail.com>
2024-08-12 11:54:43 +0200

    initial commit

---
+++ RawSearch/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace RawSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnChangeDirectory = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnChangeDirectory);
+            this.panel1.Controls.Add(this.lblStatus);
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 446);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnChangeDirectory
+            // 
+            this.btnChangeDirectory.Location = new System.Drawing.Point(12, 55);
+            this.btnChangeDirectory.Name = "btnChangeDirectory";
+            this.btnChangeDirectory.Size = new System.Drawing.Size(254, 23);
+            this.btnChangeDirectory.TabIndex = 10;
+            this.btnChangeDirectory.Text = "Choose directory...";
+            this.btnChangeDirectory.UseVisualStyleBackColor = true;
+            this.btnChangeDirectory.Click += new System.EventHandler(this.btnChangeDirectory_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 416);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(42, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Fill form";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 382);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(254, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(110, 411);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(191, 411);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text to search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 25);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(254, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(279, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(545, 446);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 446);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "RawSearch v1.0";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnChangeDirectory;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+
+    }
+}
+
---
+++ RawSearch/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RawSearch
+{
+    
+    public partial class Form1 : Form
+    {
+        private IEnumerable<Encoding> encodings = new[]
+            {
+                Encoding.Unicode,
+                Encoding.ASCII,
+                Encoding.UTF8,
+                Encoding.GetEncoding(1251)
+            };
+
+        private string _selectedDirectory = null;
+
+        private int processedCount = 0;
+        private List<FileInfo> files = null;
+        private List<MatchedFile> matchedFiles = new List<MatchedFile>();
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSearch.Text))
+            {
+                MessageBox.Show("Search criteria is empty", "Validation error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(_selectedDirectory))
+            {
+                MessageBox.Show("search directory is empty", "Validation error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (files == null)
+            {
+                DirectoryInfo directory = new DirectoryInfo(_selectedDirectory);
+                files = new List<FileInfo>();
+                IterateFolder(directory);
+                UpdateLabel();
+            }
+
+            processedCount = 0;
+            progressBar1.Maximum = files.Count;
+            listBox1.Items.Clear();
+
+            foreach (var file in files)
+            {
+                try
+                {
+                    foreach (var encoding in encodings)
+                    {
+                        byte[] criteriaBytes = encoding.GetBytes(txtSearch.Text);
+                        int criteriaBytesLength = criteriaBytes.Length;
+
+                        using (FileStream filestream = file.OpenRead())
+                        {
+                            byte[] fileBytes = new byte[filestream.Length];
+                            filestream.Read(fileBytes, 0, (int)filestream.Length);
+
+                            int matchedBytes = 0;
+                            for (int i = 0; i < fileBytes.Length; i++)
+                            {
+                                if (fileBytes[i] == criteriaBytes[matchedBytes])
+                                {
+                                    matchedBytes++;
+
+                                    if (matchedBytes == criteriaBytesLength)
+                                    {
+                                        var matchedFile = new MatchedFile() { Path = file.FullName, Position = i - matchedBytes + 1 };
+                                        matchedFiles.Add(matchedFile);
+                                        listBox1.Items.Add(matchedFile.Path + ":" + encoding.EncodingName + ":" + matchedFile.Position);
+                                        matchedBytes = 0;
+                                    }
+                                }
+                                else
+                                {
+                                    matchedBytes = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add(file.FullName + ": " + ex.Message);
+                }
+                finally
+                {
+                    progressBar1.Value = ++processedCount;
+                    progressBar1.Invalidate();
+                    Application.DoEvents();
+                    UpdateLabel();
+                }
+            }
+        }
+
+        private void btnChangeDirectory_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _selectedDirectory = folderBrowserDialog1.SelectedPath;
+                btnChangeDirectory.Text = _selectedDirectory;
+
+                DirectoryInfo directory = new DirectoryInfo(_selectedDirectory);
+                files = new List<FileInfo>();
+                IterateFolder(directory);
+                UpdateLabel();
+            }
+        }
+
+        private void IterateFolder(DirectoryInfo directory)
+        {
+            try
+            {
+                foreach (var subfiles in directory.GetFiles())
+                {
+                    files.Add(subfiles);
+                }
+            }
+            catch (Exception) { }
+
+            UpdateLabel(true);
+
+            try
+            {
+                foreach (var subdirectory in directory.GetDirectories())
+                {
+                    IterateFolder(subdirectory);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void UpdateLabel(bool isInProcess = false)
+        {
+            lblStatus.Text = string.Format("{0}/{1}{2}", processedCount, files.Count, isInProcess ? "..." : "");
+            lblStatus.Invalidate();
+            Application.DoEvents();
+        }
+    }
+
+    public class MatchedFile
+    {
+        public string Path { get; set; }
+        public int Position { get; set; }
+    }
+
+}
